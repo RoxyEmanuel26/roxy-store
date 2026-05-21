aaaa971726adb2b2c1e1273f8816f3922103f0b2
RoxyEmanuel26 <bimantara1997@yahoo.com>
2026-05-22 03:05:00 +0700

    chore: untrack scratch/ directory in git

--- scratch/check-headers.js
+++
@@ -1,5 +0,0 @@
-const https = require('https');
-
-https.get('https://www.roxystore.web.id/', (res) => {
-    console.log('Headers:', res.headers);
-});
--- scratch/list-all-images.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('scratch/desktop-shopee.html', 'utf8');
-
-const regex = /https:\/\/(?:[a-zA-Z0-9-.]+\.img\.susercontent\.com|cf\.shopee\.[a-z.]+)\/file\/[a-zA-Z0-9_-]+/gi;
-const matches = html.match(regex) || [];
-console.log('Total matches found:', matches.length);
-const unique = Array.from(new Set(matches));
-console.log('Unique image URLs found:', unique.length);
-console.log(unique);
